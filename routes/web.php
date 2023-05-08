1042f519d53aac072e3c70ad014a940d8da25908
lucacappello79 <postt79@gmail.com>
2023-05-08 17:05:17 +0200

    feat: added pages, changed routes

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,57 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-    return view('homeSweetHome');
-});
-
-Route::get('/home', function () {
 
 
     $data = [
-        "greetings" => "Hello",
-        "planet" => "World"
-
+        "greetings" => "Hello, ",
+        "planet" => "World!"
     ];
 
-    return view('homeSweetHome', $data);
+    return view('home', $data);
+});
+
+Route::get('/page2', function () {
+
+
+    $data = [
+        "greetings" => " Привет, ",
+        "planet" => "мир!"
+    ];
+
+    return view('page2', $data);
+});
+
+
+Route::get('/page3', function () {
+
+
+    $data = [
+        "greetings" => " 你好, ",
+        "planet" => " 世界！"
+    ];
+
+    return view('page3', $data);
+});
+
+Route::get('/page4', function () {
+
+
+    $data = [
+        "greetings" => "Halo, ",
+        "planet" => "dunia!"
+    ];
+
+    return view('page4', $data);
+});
+
+Route::get('/page5', function () {
+
+
+    $data = [
+        "greetings" => "Olá, ",
+        "planet" => "mundo!"
+    ];
+
+    return view('page5', $data);
 });
